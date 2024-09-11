542b4b778755a985b8eb607163b288f9a6c52248
Felipejenoy <johanordonez@unicauca.edu.co>
2024-09-11 14:53:33 -0500

    creamos database, y versel.json

--- database.js
+++ database.js
@@ -1,2 +1,12 @@
-const mongoose = requiere('mongoose');
-mongoose.set('strickQuery')
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', true);
+//mongoose.connect('mongodb://junction.proxy.rlwy.net:17976', {
+mongoose.connect('mongodb+srv://danieljm1085:djm@clusterapp.fjnwi.mongodb.net/', {
+    //useCreateIndex: true,
+    //useNewUrlParser: true,
+    //useFindAndModify: false
+
+})
+    .then(db => console.log('BD conectada'))
+    .catch(err => console.error(err));
